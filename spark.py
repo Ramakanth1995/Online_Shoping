9e4efc89d041c0a0f610af6706da078732ccf53c
ramakanth1995 <ramakanth406@gmail.com>
2022-01-14 19:58:46 +0530

    deployment to server

--- spark.py
+++ spark.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route('/newstoress.herokuapp.com')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
 	return 'Hello World'
