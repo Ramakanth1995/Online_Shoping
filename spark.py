7d24e7dc709fd5d9e5d4e12d4d81e146290b3aa2
ramakanth1995 <ramakanth406@gmail.com>
2022-01-14 21:58:17 +0530

    deployment to server

--- spark.py
+++ spark.py
@@ -1,86 +1,22 @@
-from flask import Flask, make_response, jsonify, request
-import dataset
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, render_template, request
 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
 app = Flask(__name__)
-db = dataset.connect('sqlite:///api.db')
 
-'''
-Examples:
-GET request to /api/books returns the details of all books
-POST request to /api/books creates a book with the ID 3 (As per request body)
-Sample request body -
-{
-        "book_id": "1",
-        "name": "A Game of Thrones",
-        "author": "George R. R. Martin"
-}
-GET request to /api/books/3 returns the details of book 3
-PUT request to /api/books/3 to update fields of book 3
-DELETE request to /api/books/3 deletes book 3
-'''
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return render_template('salon_home.html')
 
-table = db['books']
-
-
-def fetch_db(book_id):  # Each book scnerio
-    return table.find_one(book_id=book_id)
-
-
-def fetch_db_all():
-    books = []
-    for book in table:
-        books.append(book)
-    return books
-
-
-@app.route('/api/db_populate', methods=['GET'])
-def db_populate():
-    table.insert({
-        "book_id": "1",
-        "name": "A Game of Thrones.",
-        "author": "George R. R. Martin"
-    })
-
-    table.insert({
-        "book_id": "2",
-        "name": "Lord of the Rings",
-        "author": "J. R. R. Tolkien"
-    })
-
-    return make_response(jsonify(fetch_db_all()),
-                         200)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def api_books():
-    if request.method == "GET":
-        return make_response(jsonify(fetch_db_all()), 200)
-    elif request.method == 'POST':
-        content = request.json
-        book_id = content['book_id']
-        table.insert(content)
-        return make_response(jsonify(fetch_db(book_id)), 201)  # 201 = Created
-
-
-@app.route('/api/books/<book_id>', methods=['GET', 'PUT', 'DELETE'])
-def api_each_book(book_id):
-    if request.method == "GET":
-        book_obj = fetch_db(book_id)
-        if book_obj:
-            return make_response(jsonify(book_obj), 200)
-        else:
-            return make_response(jsonify(book_obj), 404)
-    elif request.method == "PUT":  # Updates the book
-        content = request.json
-        table.update(content, ['book_id'])
-
-        book_obj = fetch_db(book_id)
-        return make_response(jsonify(book_obj), 200)
-    elif request.method == "DELETE":
-        table.delete(id=book_id)
-
-        return make_response(jsonify({}), 204)
-
-
+# main driver function
 if __name__ == '__main__':
-    app.run(debug=True)
+
+	# run() method of Flask class runs the application
+	# on the local development server.
+	app.run()
