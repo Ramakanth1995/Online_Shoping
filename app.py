4ec27efa26a585b7c5a7383bee386d54760b9ef4
ramakanth1995 <ramakanth406@gmail.com>
2022-01-15 17:15:16 +0530

    deployment to server

--- app.py
+++ app.py
@@ -1,92 +1,93 @@
 from flask import render_template
 
-from movies import *
+from grocery import *
+from datetime import datetime
 
-grocerylist_records = None
+Grocery_tablelist_records = None
 c_names = None
-# route to get all movies
+# route to get all Grocery_tables
 @app.route('/', methods=['GET', 'POST','PUT','DELETE'])
-def get_movies():
-    global grocerylist_records,c_names
-    '''Function to get all the movies in the database'''
-    #d = jsonify({'Movies': Movie.get_all_movies(1)})
-    grocerylist_records = Movie.get_all_movies(1)
-    c_names = grocerylist_records[0].keys()
-    #return jsonify({'Movies': Movie.get_all_movies(1)})
-    return render_template('basic.html', name=grocerylist_records,column_names = c_names)
+def get_Grocery_tables():
+    global Grocery_tablelist_records,c_names
+    '''Function to get all the Grocery_tables in the database'''
+    #d = jsonify({'Grocery_tables': Grocery_table.get_all_Grocery_tables(1)})
+    Grocery_tablelist_records = Grocery_table.get_all_Grocery_tables(1)
+    print(Grocery_tablelist_records)
+    c_names = Grocery_tablelist_records[0].keys()
+    #return jsonify({'Grocery_tables': Grocery_table.get_all_Grocery_tables(1)})
+    return render_template('index.html', name=Grocery_tablelist_records,column_names = c_names)
 
 
 
-# route to get movie by id
-'''@app.route('/movies/<int:id>', methods=['GET', 'POST','PUT','DELETE'])
-def get_movie_by_id(id):
-    return_value = Movie.get_movie(id)
+# route to get Grocery_table by Item_No
+'''@app.route('/Grocery_tables/<int:Item_No>', methods=['GET', 'POST','PUT','DELETE'])
+def get_Grocery_table_by_Item_No(Item_No):
+    return_value = Grocery_table.get_Grocery_table(Item_No)
     return jsonify(return_value)'''
 
 
-# route to add new movie
-@app.route('/movies', methods=['GET', 'POST','PUT','DELETE'])
-def add_movie():
-    global grocerylist_records, c_names
-    '''Function to add new movie to our database'''
-    print(request.method,'ad')
-    request.method = "POST"
-    print(request.method, 'ad')
-    title = request.form['title']
-    print(title)
-    year = request.form['year']
-    genre = request.form['genre']
+# route to add new Grocery_table
+@app.route('/Grocery_tables', methods=['GET', 'POST','PUT','DELETE'])
+def add_Grocery_table():
+    global Grocery_tablelist_records, c_names
+
+    #Item_Name,Quantity_Remain = Quantity_Remain,Item_Cost = Item_Cost,Expiry_Date = Expiry_Date,Manufactured_By= Manufactured_By,Item_Type = Item_Type,Item_Code = Item_Code
     request.method = 'POST'
     request_data = {}
-    request_data["title"] = title
-    request_data["year"] = year
-    request_data["genre"] = genre
-    grocerylist_records = Movie.get_all_movies(1)
 
+    request_data["Item_Name"] = 'Sandwich Wheat'
+    request_data["Quantity_Remain"] = 93
+    request_data["Item_Cost"] = 9.99
+
+    request_data["Manufactured_By"] = 'Essential Everyday '
+    request_data["Item_Type"] = 'BREAD&BAKERY'
+    request_data["Item_Code"] = 'BREAD-1478523690'
+
+    Grocery_tablelist_records = Grocery_table.get_all_Grocery_tables(1)
+    print(request_data)
     #request_data = request.get_json()  # getting data from client
-    Movie.add_movie(request_data["title"], request_data["year"],
-                    request_data["genre"])
-    response = Response("Movie added", 201, mimetype='application/json')
-    return get_movies()
+    Grocery_table.add_Grocery_table(request_data["Item_Name"],request_data["Quantity_Remain"],request_data["Item_Cost"],request_data["Manufactured_By"],request_data["Item_Type"],request_data["Item_Code"])
+    response = Response("Grocery_table added", 201, mimetype='application/json')
+    return get_Grocery_tables()
 
 
-# route to update movie with PUT method
-id_value = None
-@app.route('/movies/<int:id>', methods=['GET', 'POST','PUT','DELETE'])
-def update_movie(id):
-    global id_value
-    id_value = id
-    return_value = Movie.get_movie(id)
+# route to update Grocery_table with PUT method
+Item_No_value = None
+@app.route('/Grocery_tables/<int:Item_No>', methods=['GET', 'POST','PUT','DELETE'])
+def update_Grocery_table(Item_No):
+    global Item_No_value
+    Item_No_value = Item_No
+    return_value = Grocery_table.get_Grocery_table(Item_No)
 
     return render_template('Edit.html',data = return_value[0])
 
 @app.route('/updated', methods=['GET', 'POST','PUT','DELETE'])
 def updated():
-    print(id_value,'id')
-    global grocerylist_records, c_names
+    print(Item_No_value,'Item_No')
+    global Grocery_tablelist_records, c_names
     request_data = {}
     request_data['title'] = request.form['title']
     request_data['year'] = request.form['year']
     request_data['genre'] = request.form['genre']
 
-    Movie.update_movie(id_value, request_data['title'], request_data['year'], request_data['genre'])
+    Grocery_table.update_Grocery_table(Item_No_value, request_data['title'], request_data['year'], request_data['genre'])
 
-    grocerylist_records = Movie.get_all_movies(1)
+    Grocery_tablelist_records = Grocery_table.get_all_Grocery_tables(1)
 
-    return render_template('basic.html', name=grocerylist_records,column_names = c_names)
+    return render_template('basic.html', name=Grocery_tablelist_records,column_names = c_names)
 
-# route to delete movie using the DELETE method
-@app.route('/moviess/<int:id>', methods=['GET', 'POST','PUT','DELETE'])
-def remove_movie(id):
-    '''Function to delete movie from our database'''
-    print(request.method,id)
+# route to delete Grocery_table using the DELETE method
+@app.route('/Grocery_tabless/<int:Item_No>', methods=['GET', 'POST','PUT','DELETE'])
+def remove_Grocery_table(Item_No):
+    '''Function to delete Grocery_table from our database'''
+    print(request.method,Item_No)
 
-    if id ==1 :
-        response = Response("Id 1 we could not Deleted", status=200, mimetype='application/json')
+    if Item_No ==1 :
+        response = Response("Item_No 1 we could not Deleted", status=200, mimetype='application/json')
     else:
-        Movie.delete_movie(id)
+        Grocery_table.delete_Grocery_table(Item_No)
 
-    return get_movies()
+    return get_Grocery_tables()
 
 
 if __name__ == "__main__":
---
+++ grocery.py
@@ -0,0 +1,61 @@
+from settings import *
+import json
+# Initializing our database
+db = SQLAlchemy(app)
+
+
+# the class Grocery_table will inherit the db.Model of SQLAlchemy
+class Grocery_table(db.Model):
+    __tablename__ = 'Grocery_tables'  # creating a table name #Grocery_tablelist
+    Item_No = db.Column(db.Integer, primary_key=True)  # this is the primary key
+    Item_Name = db.Column(db.String(80), nullable=False)
+    Quantity_Remain  = db.Column(db.Integer)
+    Item_Cost  = db.Column(db.Integer)
+    Manufactured_By  = db.Column(db.String(80), nullable=False)
+    Item_Type  = db.Column(db.String(80), nullable=False)
+    Item_Code  = db.Column(db.String(80), nullable=False)
+
+    def json(self):
+
+        return {'Item_No': self.Item_No, 'Item_Name': self.Item_Name,
+                'Quantity_Remain': self.Quantity_Remain, 'Item_Cost': self.Item_Cost,
+                'Manufactured_By': self.Manufactured_By,'Item_Type': self.Item_Type,'Item_Code': self.Item_Code}
+        # this method we are defining will convert our output to json
+
+    def add_Grocery_table(_Item_Name,_Quantity_Remain,_Item_Cost,_Manufactured_By,_Item_Type,_Item_Code):
+        '''function to add Grocery_table to database using _title, _year, _genre
+        as parameters'''
+        # creating an instance of our Grocery_table constructor
+        new_Grocery_table = Grocery_table(Item_Name = _Item_Name,Quantity_Remain = _Quantity_Remain,Item_Cost = _Item_Cost,Manufactured_By= _Manufactured_By,Item_Type = _Item_Type,Item_Code = _Item_Code)
+        db.session.add(new_Grocery_table)  # add new Grocery_table to database session
+        db.session.commit()  # commit changes to session
+
+    def get_all_Grocery_tables(self):
+        '''function to get all Grocery_tables in our database'''
+        #db.create_all()
+        return [Grocery_table.json(grocery_table) for grocery_table in Grocery_table.query.all()]
+
+        #return Grocery_table.query.all()
+
+    def get_Grocery_table(_Item_No):
+        '''function to get Grocery_table using the Item_No of the Grocery_table as parameter'''
+        return [Grocery_table.json(Grocery_table.query.filter_by(Item_No=_Item_No).first())]
+        # Grocery_table.json() coverts our output to json
+        # the filter_by method filters the query by the Item_No
+        # the .first() method displays the first value
+
+    def update_Grocery_table(_Item_No, _title, _year, _genre):
+        '''function to update the details of a Grocery_table using the Item_No, title,
+        year and genre as parameters'''
+        Grocery_table_to_update = Grocery_table.query.filter_by(Item_No=_Item_No).first()
+        Grocery_table_to_update.title = _title
+        Grocery_table_to_update.year = _year
+        Grocery_table_to_update.genre = _genre
+        db.session.commit()
+
+    def delete_Grocery_table(_Item_No):
+        '''function to delete a Grocery_table from our database using
+           the Item_No of the Grocery_table as a parameter'''
+        Grocery_table.query.filter_by(Item_No=_Item_No).delete()
+        # filter by Item_No and delete
+        db.session.commit()  # commiting the new change to our database
